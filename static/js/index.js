e7b5dfbffbb555d401d746c103166d6a4a294f36
Kaspar Moberg <kaspeti123@gmail.com>
2024-06-23 14:48:05 +0200

    added documentation and removed unused code

--- static/js/index.js
+++ static/js/index.js
@@ -1,22 +1,24 @@
+// Set the amount of color bins used when showing the answer
+// and the distance measure to use ("centroid", "")
 const colorBins = 10
 const color = d3.scaleSequential([0, colorBins], d3.interpolateBlues);
 const distanceMeasure = "centroid"
 
 let answerName
-let colorIndices = {}
-let activeGame = false
+let colorIndices = {}   // The color indices for the countries. Will be in the range 0-colorBins
+let activeGame = false  // Used for preventing input inbetween games
 
-let distances = {}
+let distances = {}      // The distance from each country to each other country
 
 let alertText = document.getElementById("alert_text")
 
-let testAnswer
-
 function guessCountry(event) {
+  // Enter key
   if (event.keyCode !== 13) {
     return
   }
 
+  // If input is given inbetween games alert that the game is not active
   if (!activeGame) {
     alertText.style.opacity = 1
     alertText.innerText = "Game is not active, start a new game using by pressing 'New Game'"
@@ -24,24 +26,22 @@ function guessCountry(event) {
   }
 
   const country = document.getElementById("input")
-
   if (country.value === "") {
     return
   }
 
   const countryObject = d3.select(`#map svg g .unit-${country.value}`)
 
-  if (!countryObject.empty()) {
+  if (!countryObject.empty()) { // Country exist
     if (countryObject.datum().properties.name === answerName) {
       countryObject.style("fill", "green")
       document.getElementById("color_tooltip").style.backgroundColor = "green"
-    } else {
+    } else { 
       const c = color(colorIndices[countryObject.datum().properties.name])
       countryObject.style("fill", c)
       document.getElementById("color_tooltip").style.backgroundColor = c
     }
-
-  } else {
+  } else { // Country does not exist
     alertText.style.opacity = 1
     alertText.innerText = `No country/area with called ${country.value}`
   }
@@ -50,6 +50,8 @@ function guessCountry(event) {
 }
 
 
+// Initalizes the game by clearing the country colors, choosing a random country as answer and
+// populating the colorIndices object according to the distances
 function initGame() {
   colorIndices = {}
   const countries = d3.selectAll("#map svg g .unit")
@@ -57,32 +59,13 @@ function initGame() {
 
   let answer = d3.select(node)
   answerName = answer.datum().properties.name
-  // const answerCenter = d3.geoCentroid(answer.datum())
 
-  // let distances = {}
-  countries.each(function(d) {
-    // if (d.properties.name !== answer.datum().properties.name) {
-    //   const countryCentroid = d3.geoCentroid(d)
-    //   let distSqrd
-    //
-    //   if (distanceMeasure === "centroid") {
-    //     distSqrd = Math.pow(answerCenter[0] - countryCentroid[0], 2) + Math.pow(answerCenter[1] - countryCentroid[1], 2)
-    //   } else {
-    //     distSqrd = calculateDistance(answer.datum(), d)
-    //   }
-    //   distances[d.properties.name] = Math.sqrt(distSqrd)
-    // }
-
+  countries.each(function() {
     d3.select(this).style("fill", "#ccc")
   })
 
-  // const [minDistance, maxDistance] = Object.entries(distances[answerName]).reduce(
-  //   ([min, max], [_, value]) => [Math.min(min, value), Math.max(max, value)],
-  //   [Infinity, -Infinity]
-  // );
-
   const dists = Object.values(distances[answerName])
-  const powBase = 0.3
+  const powBase = 0.3   // Change this for different gradient. Default distances without power looks strange
   const powDists = dists.map(d => Math.pow(d, powBase))
   const minDistance = Math.min(...powDists) 
   const maxDistance = Math.max(...powDists)
@@ -99,10 +82,11 @@ function initGame() {
   alertText.style.opacity = 0
   activeGame = true
 
-  testAnswer = answer
 }
 
 
+// Show the answer by coloring all the countries according to
+// their color index, and the answer in green
 function showAnswer() { 
   activeGame = false
   alertText.style.opacity = 1
@@ -125,29 +109,8 @@ function showAnswer() {
 }
 
 
-function calculateDistance(d1, d2) {
-  let d1Coords = d1.geometry.coordinates.flat()
-  let d2Coords = d2.geometry.coordinates.flat()
-
-  if (d1Coords[0][0].length) {
-    d1Coords = [].concat(...d1Coords) 
-  }
-
-  if (d2Coords[0][0].length) {
-    d2Coords = [].concat(...d2Coords)
-  }
-
-  let minDist = Infinity
-  d1Coords.forEach(function(c1) {
-    d2Coords.forEach(function(c2) {
-      minDist = Math.min(minDist, Math.pow(c1[0] - c2[0], 2) + Math.pow(c1[1] - c2[1], 2))
-    })
-  })
-
-  return minDist
-}
-
-
+// Initalize the map using the d3-geomap library
+// Import the precalculated distances
 async function initMap() {
   let map = d3.geomap()
     .geofile("/static/data/topojson.json")
